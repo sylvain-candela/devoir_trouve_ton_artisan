3b7b218042778bb260901c08de437c8ecaea8a95
sylvain candela <sylvain.candela@gmail.com>
2026-04-24 15:45:43 +0200

    adaptation pour alwaysdata

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,5 @@ const app = express();
 
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
+    console.error("Erreur de connexion à la base de données :", err);
 });
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 app.get('/api/artisans', async (req, res) => {
     try {
@@ -15,10 +17,9 @@ app.get('/api/artisans', async (req, res) => {
         JOIN specialites s ON a.id_specialite = s.id_specialite
         JOIN categories c ON s.id_categorie = c.id_categorie 
         `;
-
         const [rows] = await db.query(query);
         res.json(rows);
-    } catch {
+    } catch (err) {
         console.error("Erreur SQL :", err);
         res.status(500).json({ error: "Impossible de récupérer les artisans"});
     }
@@ -34,8 +35,6 @@ app.get('/api/artisans/top', async (req, res) => {
             LEFT JOIN specialites s ON a.id_specialite = s.id_specialite
             WHERE a.top = 1
         `);
-        
-        console.log("Données avec spécialité :", rows[0]);
         res.json(rows);
     } catch (err) {
         console.error(err);
@@ -52,28 +51,23 @@ app.get('/api/artisans/:id', async (req, res) => {
             JOIN specialites s ON a.id_specialite = s.id_specialite
             JOIN categories c ON s.id_categorie = c.id_categorie
             WHERE a.id_artisan = ?`;
-
         const [rows] = await db.query(query, [id]);
         res.json(rows);
     } catch (err) {
         console.error("Erreur SQL :", err);
         res.status(500).json({ error: "Erreur serveur" });
-    };
-
+    }
 });
 
-
-
-app.get('/api/artisans/id_specialite' , async (req,res) => {
+app.get('/api/artisans/id_specialite', async (req, res) => {
     try {
         const [rows] = await db.query(
             `SELECT 
                 a.*, 
                 s.nom_specialite
             FROM specialites a
-            LEFT JOIN specialites s ON  s.id_categorie`
+            LEFT JOIN specialites s ON s.id_categorie`
         );
-        console.log("Données avec id_catégorie et nom_specialite :", rows[0]);
         res.json(rows);
     } catch (err) {
         console.error(err);
@@ -84,16 +78,15 @@ app.get('/api/artisans/id_specialite' , async (req,res) => {
 app.get('/api/categories', async (req, res) => {
     try {
         const [rows] = await db.query("SELECT id_categorie, nom_categorie FROM categories");
-        console.log("Catégories envoyées :", rows); // Vérifie ton terminal Node
         res.json(rows);
     } catch (err) {
         console.error("Erreur SQL :", err.message);
         res.status(500).json({ error: err.message });
     }
 });
+
 app.get('/api/artisans/category/:id', async (req, res) => {
     const categoryId = req.params.id;
-    
     try {
         const [rows] = await db.query(
             `SELECT a.*, s.nom_specialite, c.nom_categorie 
@@ -109,8 +102,15 @@ app.get('/api/artisans/category/:id', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+app.use((req, res, next) => {
+    if (req.url.startsWith('/api')) {
+        return next();
+    }
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8100;
+
 app.listen(PORT, () => {
-    console.log(`Serveur démarre sur http://localhost:${PORT}`);
-    console.log(`test des données sur http://localhost:${PORT}/api/artisans`);
+    console.log(`Serveur démarre sur le port : ${PORT}`);
 });
